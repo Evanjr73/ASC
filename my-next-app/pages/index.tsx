b387cdb6402888babd4c1dffc13aad0043535ec3
Evan junior <evsioljr@gmail.com>
2022-11-07 20:54:58 -0300

    começando projeto da associação de surf

---
+++ my-next-app/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Nave from '../src/nav'
+
+export default function Home() {
+  return (
+    <div >
+      <Nave></Nave>
+    </div>
+  )
+}
---
+++ my-next-app/src/nav.tsx
@@ -0,0 +1,15 @@
+import { ReactDOM } from "react"
+import { ReactElement } from "react"
+
+
+
+
+export default function Nave(){
+    return(
+        <div id="navdiv1">
+            <p>ola mundo</p>
+        </div>
+    )
+}
+
+ 
