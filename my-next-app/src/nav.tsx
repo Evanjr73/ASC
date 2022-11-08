7cb6f8bf6a9d7f23e7b1c7409974236e86bd713a
Evan junior <evsioljr@gmail.com>
2022-11-07 21:30:08 -0300

    estilizando nave

--- my-next-app/src/nav.tsx
+++ my-next-app/src/nav.tsx
@@ -1,5 +1,8 @@
 import { ReactDOM } from "react"
 import { ReactElement } from "react"
+import Image from "next/image"
+import image from './img/carangueijo.png'
+// import navstyles from '../styles/nav.scss'
 
 
 
@@ -7,7 +10,10 @@ import { ReactElement } from "react"
 export default function Nave(){
     return(
         <div id="navdiv1">
-            <p>ola mundo</p>
+           <Image src={image} alt= ""/>
+            
+            <input type="---------" />
+
         </div>
     )
 }
