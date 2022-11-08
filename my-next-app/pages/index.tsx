1b07775e40fba34ca58935bccdba391f33dbbfff
Evan junior <evsioljr@gmail.com>
2022-11-07 21:58:34 -0300

    estilizando a apresentação

--- my-next-app/pages/index.tsx
+++ my-next-app/pages/index.tsx
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Nave from '../src/nav'
+import Apre from '../src/apre'
 
 
 export default function Home() {
   return (
     <div >
       <Nave></Nave>
+      <Apre></Apre>
     </div>
   )
 }
---
+++ my-next-app/src/apre.tsx
@@ -0,0 +1,23 @@
+import { ReactDOM } from "react"
+import { ReactElement } from "react"
+import Image from "next/image"
+
+
+
+
+
+export default function Apre(){
+    return(
+
+        <div id="aprediv1">
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
