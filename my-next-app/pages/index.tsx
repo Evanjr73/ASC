0b8a9a7b4214f24d16bdcf9bb07dfb505867e65b
Evan junior <evsioljr@gmail.com>
2022-11-09 20:54:59 -0300

    estilizando o grid

--- my-next-app/pages/index.tsx
+++ my-next-app/pages/index.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Nave from '../src/nav'
 import Apre from '../src/apre'
-
+import Categorias from '../src/categorias'
 
 export default function Home() {
   return (
     <div >
       <Nave></Nave>
       <Apre></Apre>
+      <Categorias></Categorias>
     </div>
   )
 }
--- my-next-app/src/categorias.tsx
+++ my-next-app/src/categorias.tsx
@@ -7,7 +7,14 @@ export default function Categorias(){
 
         <div id="categoriasdiv1">
 
-                
+                <div className="categorias">1</div>
+                <div className="categorias">2</div>
+                <div className="categorias">3</div>
+                <div className="categorias">4</div>
+                <div className="categorias">5</div>
+                <div className="categorias">6</div>
+                <div className="categorias">7</div>
+                <div className="categorias">8</div>
 
 
 
