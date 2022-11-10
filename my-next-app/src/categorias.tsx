194359585f89b940e8a85541cf73fd351bc7ac17
Evan junior <evsioljr@gmail.com>
2022-11-09 21:22:40 -0300

    nomiando divs

--- my-next-app/src/categorias.tsx
+++ my-next-app/src/categorias.tsx
@@ -8,17 +8,12 @@ export default function Categorias(){
             
             <div id="categoriasdiv2">
 
-                <div className="categorias">1</div>
-                <div className="categorias">2</div>
-                <div className="categorias">3</div>
-                <div className="categorias">4</div>
-                <div className="categorias">5</div>
-                <div className="categorias">6</div>
-                <div className="categorias">7</div>
-                <div className="categorias">8</div>
-
-
-
+                <div className="categorias">RANKING</div>
+                <div className="categorias">EVENTOS</div>
+                <div className="categorias">FOTOS</div>
+                <div className="categorias">ASC</div>
+                <div className="categorias"> INFORMAÇÕES</div>
+                <div className="categorias">PICOS</div>
             </div>
 
 
