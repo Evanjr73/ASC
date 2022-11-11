ef1f438127d7edb991bfe28317bd4e1df7b53d7f
Evan junior <evsioljr@gmail.com>
2022-11-11 12:32:02 -0300

    trabalhando o responsivo

--- my-next-app/pages/noticias.tsx
+++ my-next-app/pages/noticias.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Nave from "../src/nav";
 export default function Noticias(){
     return(
-        <div>
-            ola mundo
-        </div>
+        
+        <Nave></Nave>
+        
+        
     )
 }
