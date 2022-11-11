6dc1cb53dea06005476ccd1c4dd6f260ed4045ac
Evan junior <evsioljr@gmail.com>
2022-11-10 21:08:57 -0300

    criando o footer

---
+++ my-next-app/src/footer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Image from "next/image";
+
+
+export default function Footer(){
+    return(
+
+        <div id="footer1">
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+    )
+}
