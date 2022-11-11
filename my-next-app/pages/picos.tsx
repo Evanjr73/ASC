4648e1df6976138af9ab36d661002ba98b9d8b01
Evan junior <evsioljr@gmail.com>
2022-11-11 15:20:43 -0300

    add page picos

---
+++ my-next-app/pages/picos.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Image from "next/image";
+
+
+
+
+
+
+
+
+
+
+
+ export default function Picos(){
+    return(
+
+
+        <div id="picos1">
+
+
+
+            <div className="pico">1</div>
+            <div className="pico">2</div>
+            <div className="pico">3</div>
+            <div className="pico">4</div>
+            <div className="pico">5</div>
+            <div className="pico">6</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+    )
+ }
