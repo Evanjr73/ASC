cdece80cf5ec865b0b7748b6ab87810d3da3e3c9
Evan junior <evsioljr@gmail.com>
2022-11-11 22:57:30 -0300

    estilização de noticias

--- my-next-app/pages/noticias.tsx
+++ my-next-app/pages/noticias.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Nave from "../src/nav";
+import Noticia from "../src/noticia";
+
+
+
+
+
 export default function Noticias(){
     return(
         
-        <Nave></Nave>
         
+        <div>
+            <Nave></Nave>
+
+
+            <div id="bodyy">
+
+                <Noticia></Noticia>
+
+
+            </div>
+
+
+
+
+
+        </div>
         
     )
 }
---
+++ my-next-app/src/noticia.tsx
@@ -0,0 +1,16 @@
+import { Style } from "util"
+import style from '../styles/noticias.module.css'
+
+
+
+
+
+
+
+export default function Noticia(){
+    return(
+        <div id="noticiadiv1">
+            
+        </div>
+    )
+}
