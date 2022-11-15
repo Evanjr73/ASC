87f17bfeb02e2ecc7147ca7aa8c07d1cd1ba13a0
Evan junior <evsioljr@gmail.com>
2022-11-14 22:48:28 -0300

    estilizando componets apre

--- my-next-app/src/categorias.tsx
+++ my-next-app/src/categorias.tsx
@@ -1,7 +1,15 @@
 import { ReactDOM } from "react";
 import { ReactElement } from "react";
 import Image from "next/image";
-import asc from './img/asc1.png'
+import ranking from './img/instagram.png'
+import fotos from './img/driver.png'
+import eventos from './img/baner.jpeg'
+import informações from './img/coqueiros.jpg'
+import picos from './img/ondas.png'
+import asc from './img/carangueijo.png'
+
+
+
 
 export default function Categorias(){
     return(
@@ -10,16 +18,31 @@ export default function Categorias(){
             <div id="categoriasdiv2">
 
                 <div className="categorias" id="cate1">
-                    
+                    <Image src={ranking} alt= "" className="fotocate"/>
                     RANKING
                 </div>
-                <div className="categorias" id="cate2">EVENTOS</div>
-                <div className="categorias" id="cate3">FOTOS</div>
-                <div className="categorias" id="cate4">ASC</div>
-                <div className="categorias" id="cate5"> INFORMAÇÕES</div>
-                <div className="categorias" id="cate6">PICOS</div>
-            </div>
+                <div className="categorias" id="cate2">
+                    <Image src={eventos} alt= "" className="fotocate"/>
+                    EVENTOS
+                </div>
+                <div className="categorias" id="cate3">
+                    <Image src={fotos} alt= "" className="fotocate"/>
+                    FOTOS</div>
 
+                <div className="categorias" id="cate4">
+                    <Image src={asc} alt= "" className="fotocate"/>                    
+                    ASC</div>
+                <div className="categorias" id="cate5">
+                    
+                    <Image src={informações} alt= "" className="fotocate"/>
+                    
+                     INFORMAÇÕES</div>
+                <div className="categorias" id="cate6">
+                    
+                    <Image src={picos} alt= "" className="fotocate"/>
+                    PICOS</div>
+            </div>
+           
 
         </div>
 
