8b299d05afab3fe29f90f4d8bbe27e8d713da434
Evan junior <evsioljr@gmail.com>
2022-11-19 22:15:19 -0300

    categorias em module

--- my-next-app/src/categorias.tsx
+++ my-next-app/src/categorias.tsx
@@ -7,37 +7,37 @@ import eventos from './img/baner.jpeg'
 import informações from './img/coqueiros.jpg'
 import picos from './img/ondas.png'
 import asc from './img/carangueijo.png'
-
+import cate from '../styles/categorias.module.css'
 
 
 
 export default function Categorias(){
     return(
-        <div id="categoriasdiv1" >
+        <div id={cate.categoriasdiv1} >
             
-            <div id="categoriasdiv2">
+            <div id={cate.categoriasdiv2}>  
 
-                <div className="categorias" id="cate1">
+                <div className={cate.categorias} id={cate.cate1}>
                     <Image src={ranking} alt= "" className="fotocate"/>
                     <p>RANKING</p>
                 </div>
-                <div className="categorias" id="cate2">
+                <div className={cate.categorias} id={cate.cate2}>
                     <Image src={eventos} alt= "" className="fotocate"/>
                     <p>EVENTOS</p>
                 </div>
-                <div className="categorias" id="cate3">
+                <div className={cate.categorias} id={cate.cate3}>
                     <Image src={fotos} alt= "" className="fotocate"/>
                     <p>FOTOS</p></div>
 
-                <div className="categorias" id="cate4">
+                <div className={cate.categorias} id={cate.cate4}>
                     <Image src={asc} alt= "" className="fotocate"/>                    
                     <p>ASC</p></div>
-                <div className="categorias" id="cate5">
+                <div className={cate.categorias} id={cate.cate5}>
                     
                     <Image src={informações} alt= "" className="fotocate"/>
                     
                      <p>INFORMAÇÕES</p></div>
-                <div className="categorias" id="cate6">
+                <div className={cate.categorias} id={cate.cate6}>
                     
                     <Image src={picos} alt= "" className="fotocate"/>
                     PICOS
