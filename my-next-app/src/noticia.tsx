432be8e058e32725f71955080b9fd89239e8db7b
Evan junior <evsioljr@gmail.com>
2022-11-21 22:02:00 -0300

    excluindo styles

--- my-next-app/src/noticia.tsx
+++ my-next-app/src/noticia.tsx
@@ -1,9 +1,9 @@
-import style from '../styles/noticias.module.scss'
+import styl from '../styles/noticias.module.css'
 
 
 export default function Noticia(){
     return(
-        <div className={style.noticiass}>
+        <div className={styl.noticiass}>
             <div>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, provident expedita? Nam numquam nihil officia ex laborum quam laboriosam saepe, omnis odio tenetur! Officia, accusantium! Consequuntur optio error voluptatum vitae!
             </div>
