21e9b5250ad18b50b678d1467b5f402e80053b0e
Evan junior <evsioljr@gmail.com>
2022-12-09 20:53:49 -0300

    new comit

--- my-next-app/src/categorias.tsx
+++ my-next-app/src/categorias.tsx
@@ -6,7 +6,7 @@ import informações from './img/coqueiros.jpg'
 import picos from './img/ondas.png'
 import asc from './img/carangueijo.png'
 import cate from '../styles/categorias.module.css'
-
+import Link from "next/link";
 
 
 export default function Categorias(){
@@ -16,29 +16,44 @@ export default function Categorias(){
             <div id={cate.categoriasdiv2}>  
 
                 <div className={cate.categorias} id={cate.cate1}>
-                    <Image src={ranking} alt= "" className={cate.fotocate}/>
-                    <p>RANKING</p>
+                    <Link href="../pages/ranking">
+                        <Image src={ranking} alt= "" className={cate.fotocate}/>
+                    <p>RANKING</p></Link>
                 </div>
                 <div className={cate.categorias} id={cate.cate2}>
-                    <Image src={eventos} alt= "" className={cate.fotocate}/>
-                    <p>EVENTOS</p>
+
+                    <Link href="">
+                        <Image src={eventos} alt= "" className={cate.fotocate}/>
+                        <p>EVENTOS</p>
+                    </Link>
+                    
                 </div>
                 <div className={cate.categorias} id={cate.cate3}>
-                    <Image src={fotos} alt= "" className={cate.fotocate}/>
-                    <p>FOTOS</p></div>
+                    <Link href="">
+                        <Image src={fotos} alt= "" className={cate.fotocate}/>
+                        <p>FOTOS</p>
+                        
+                        </Link> </div>
 
                 <div className={cate.categorias} id={cate.cate4}>
-                    <Image src={asc} alt= "" className={cate.fotocate}/>                    
-                    <p>ASC</p></div>
+                    <Link href="">
+                        <Image src={asc} alt= "" className={cate.fotocate}/>                    
+                        <p>ASC</p>
+                    </Link>
+                    </div>
                 <div className={cate.categorias} id={cate.cate5}>
-                    
-                    <Image src={informações} alt= "" className={cate.fotocate}/>
-                    
-                     <p>INFORMAÇÕES</p></div>
+                    <Link href="">
+                        <Image src={informações} alt= "" className={cate.fotocate}/>
+                        
+                        <p>INFORMAÇÕES</p>
+                    </Link>
+                    </div>
                 <div className={cate.categorias} id={cate.cate6}>
                     
-                    <Image src={picos} alt= "" className={cate.fotocate}/>
-                    PICOS
+                    <Link href="/picos">
+                        <Image src={picos} alt= "" className={cate.fotocate}/>
+                        <p> PICOS</p>
+                    </Link>
                     
                 </div>
             </div>
